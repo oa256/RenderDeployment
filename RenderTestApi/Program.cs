0aa12ed8049e501df7288e40e09968da66adbf8b
Olaoluwa Akinlade <olaoluwaakinlade@globusbank.com>
2025-06-23 23:00:25 +0100

    Added render port

--- RenderTestApi/Program.cs
+++ RenderTestApi/Program.cs
@@ -13,17 +13,25 @@ builder.Services.AddAutoMapper(typeof(StudentMapperProfile));
 
 var app = builder.Build();
 
+
+app.MapGet("/", () => "Hello from Render!");
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
+/*if (app.Environment.IsDevelopment())
+{*/
+app.UseSwagger();
     app.UseSwaggerUI();
-}
+/*}*/
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Set Render.com port
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
+
 
 app.Run();
